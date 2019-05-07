ea996a2c9b13431c6c878590047a46efce7c289a
Dawn Joseph <dj@dawns-mbp.bmcc.cuny.edu>
2019-05-07 16:28:02 -0400

    changes

--- Final/js/script.js
+++ Final/js/script.js
@@ -123,3 +123,21 @@ rightBtn.addEventListener('mouseleave', stopMovement);
 
 
 
+//Sticky top
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("sticky").style.padding = "30px 10px";
+    document.getElementById("header-name").style.fontSize = "25px";
+  } else {
+    document.getElementById("sticky").style.padding = "80px 10px";
+    document.getElementById("header-name").style.fontSize = "35px";
+  }
+}
+
+
+
+
